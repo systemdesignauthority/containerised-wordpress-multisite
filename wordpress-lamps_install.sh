dd1c0b4c8077287efe022223bcd564aee557fe97
systemdesignauthority <suedunome+sda@gmail.com>
2021-04-11 16:26:11 +0100

    cleanup

--- wordpress-lamps_install.sh
+++ wordpress-lamps_install.sh
@@ -12,6 +12,7 @@ function genpw {
 #echo "This program will setup wordpress-lamps for one domain eg: yourdomain.com or yourdomain.co.uk"
 #echo -n "Enter you Domain and press [ENTER]: "
 #read domain
+domain="systemdesignauthority.com"
 
 # Ensure given domain is resolving to this computer's public IP
 #thisPublicIP=$(dig @resolver4.opendns.com myip.opendns.com +short)
@@ -28,6 +29,18 @@ function genpw {
 #echo "MYSQL_USER="$(genpw) >> .env
 #echo "MYSQL_PASSWORD="$(genpw) >> .env
 #echo "DOMAIN="$domain >> .env
+
+# Ensure port forwarding is implemented by requesting certificates using certbot
+# Create docker-compose and nginx configurations using steps 1-4 from https://www.digitalocean.com/community/tutorials/how-to-install-wordpress-with-docker-compose
+# docker-compose
+rm -f docker-compose.yml
+sed "s/this_domain/$domain/g" docker-compose_staging.yml > docker-compose.yml
+# nginx
+rm -f nginx-conf/nginx.conf
+sed "s/this_domain/$domain/g" nginx-conf/nginx_staging.conf > nginx-conf/nginx.conf
+
+
+
 
 echo "the end"
 
